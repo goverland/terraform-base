9431ec41c494f8d01e723d72f24046337cfba393
Geirmund Øverland <geirmund.overland@tietoevry.com>
2024-07-10 16:07:33 +0200

    corrected contcat

--- modules/app1/main.tf
+++ modules/app1/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-    name                     = substr(replace("${var.subscription_name}", "-", ""), "storagetestgo")
+    name = substr(replace(var.subscription_name, "-", "") + "storagetestgo", 0, 24)
     resource_group_name      = azurerm_resource_group.rg.name
     location                 = azurerm_resource_group.rg.location
     account_tier             = "Standard"
