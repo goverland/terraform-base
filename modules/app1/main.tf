6ecf3408c8c1029d68895c3fb4dafba8ed406783
Geirmund Øverland <geirmund.overland@tietoevry.com>
2024-07-10 22:06:20 +0200

    Added shared config to app1 and sub-1

--- modules/app1/main.tf
+++ modules/app1/main.tf
@@ -1,31 +1,38 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = ">= 3.103.1"
     }
   }
 }
 
 resource "azurerm_resource_group" "rg" {
-    name     = "test-terraform-dev-rg"
-    location = "norwayeast"
+  name     = "test-terraform-dev-rg"
+  location = "norwayeast"
 }
 
 resource "azurerm_storage_account" "storage" {
-    name = substr(format("%s%s", replace(var.subscription_name, "-", ""), "storageapp1go"), 0, 24)
-    resource_group_name      = azurerm_resource_group.rg.name
-    location                 = azurerm_resource_group.rg.location
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
-    enable_https_traffic_only = true
+  name                      = substr(format("%s%s", replace(var.subscription_name, "-", ""), "storageapp1go"), 0, 24)
+  resource_group_name       = azurerm_resource_group.rg.name
+  location                  = azurerm_resource_group.rg.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  enable_https_traffic_only = true
 }
 
 resource "azurerm_storage_container" "container" {
-  for_each = toset(var.customer_names)
+  for_each              = toset(var.customer_names)
   name                  = each.value
   storage_account_name  = azurerm_storage_account.storage.name
   container_access_type = "private"
 }
 
+resource "azurerm_app_configuration_key" "app1_config_key" {
+  configuration_store_id = var.shared_app_config_id
+  key                    = "app1-conf-key1"
+  label                  = "somelabel"
+  value                  = "a test"
+
+}
 
--- modules/app1/variables.tf
+++ modules/app1/variables.tf
@@ -7,3 +7,9 @@ variable "subscription_name" {
     description = "Subscription name used to tag resources"
     type        = string 
 }
+
+variable "shared_app_config_id" {
+  description = "The ID of the shared app configuration"
+  type        = string
+  
+}
