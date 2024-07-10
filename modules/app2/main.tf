111f1c7c5d513513cfb3d8c66cc5e874f624b5ec
Geirmund Øverland <geirmund.overland@tietoevry.com>
2024-07-10 15:16:16 +0200

    Removed provider and backend from modules

--- modules/app2/main.tf
+++ modules/app2/main.tf
@@ -1,16 +1,3 @@
-terraform {
-    required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.103.1"
-    }
-  }
-}
-
-provider "azurerm" {
-    features {}
-}
-
 resource "azurerm_resource_group" "rg" {
     name     = "test-terraform-dev-rg"
     location = "norwayeast"
