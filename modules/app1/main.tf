1035eb9ac76bfda85d7dc9c15b86f36e0493ff00
Geirmund Øverland <geirmund.overland@tietoevry.com>
2024-07-10 16:04:12 +0200

    removed name format for storage account

--- modules/app1/main.tf
+++ modules/app1/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-    name                     = substr(format("%s%s%s", replace("${var.subscription_name}", "-", ""), "storagetestgo"), 0, 24)
+    name                     = substr(replace("${var.subscription_name}", "-", ""), "storagetestgo")
     resource_group_name      = azurerm_resource_group.rg.name
     location                 = azurerm_resource_group.rg.location
     account_tier             = "Standard"
